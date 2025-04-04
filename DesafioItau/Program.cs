8cef401761cb5eede1ca6b108a3a88a8a28b546c
Gabriel Novais Maia <gabriel.gnm@gmail.com>
2025-04-04 19:52:09 -0300

    Corrigir codificação dos arquivos para UTF-8

--- DesafioItau/Program.cs
+++ DesafioItau/Program.cs
@@ -1,31 +1,1 @@
-﻿using DesafioItaú;
-using DesafioItaú.Dto;
-using DesafioItaú.Dto.Validators;
-using FluentValidation;
-
-var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-
-builder.Services.AddControllers();
-
-builder.Services.AddScoped<IValidator<TrasacaoDto>, TrasacaoDtoValidator>();
-builder.Services.AddSingleton<TransacaoSingleton>();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.MapOpenApi();
-}
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
+﻿using DesafioItaú; using DesafioItaú.Dto; using DesafioItaú.Dto.Validators; using FluentValidation;  var builder = WebApplication.CreateBuilder(args);  // Add services to the container.  builder.Services.AddControllers();  builder.Services.AddScoped<IValidator<TrasacaoDto>, TrasacaoDtoValidator>(); builder.Services.AddSingleton<TransacaoSingleton>(); // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi builder.Services.AddOpenApi();  var app = builder.Build();  // Configure the HTTP request pipeline. if (app.Environment.IsDevelopment()) {     app.MapOpenApi(); }  app.UseHttpsRedirection();  app.UseAuthorization();  app.MapControllers();  app.Run(); 
